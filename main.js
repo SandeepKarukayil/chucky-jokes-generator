e1f9815548d72b48e0e25a72f49a227c05e4d290
SandeepKarukayil <115607146+SandeepKarukayil@users.noreply.github.com>
2024-08-29 10:40:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let h1 = document.querySelector('.quote')
+let btn = document.querySelector('button')
+btn.addEventListener('click',reload)
+function reload(){
+    window.location.reload()
+}
+let url = `https://zenquotes.io/api/quotes`
+ fetch(url)
+.then((res)=>res.json())
+.then((data)=>{
+    console.log(data)
+    // img.src=data.images[0].url
+   h1.textContent = data[0].q   
+})
+.catch((err)=>{
+    console.log(err)
+})
