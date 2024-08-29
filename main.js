d5169d51d5c15290f0f334a00a661f40d21bd35c
SandeepKarukayil <115607146+SandeepKarukayil@users.noreply.github.com>
2024-08-29 12:29:02 +0530

    kanye rest quote

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 let h1 = document.querySelector('.quote')
- let img = document.querySelector('img')
+ 
 let btn = document.querySelector('button')
 btn.addEventListener('click',reload)
 function reload(){
     window.location.reload()
 } 
-let url = `https://api.chucknorris.io/jokes/random`
+let url = `https://api.kanye.rest`
  fetch(url)
 .then((res)=>res.json())
 .then((data)=>{
-    console.log(data.value)
-    img.src=data.icon_url
-   h1.textContent = data.value   
+    console.log(data)
+ h1.textContent = data.quote   
 })
 .catch((err)=>{
     console.log(err)
